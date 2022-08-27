e48e78d6e66bb256a3c7bba9d01e939f1f107a45
Christopher Hileman <christopherhileman89@gmail.com>
2022-08-26 22:31:29 -0400

    Add selectors and functions for buttons

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const numbers = document.querySelectorAll('.number');
+const screen = document.querySelector('#screen-output');
+const zero = document.querySelector('#zero');
+const clear = document.querySelector('#clear');
+const period = document.querySelector('#period');
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -31,14 +37,49 @@ function operate(operand, num1, num2) {
     }
 }
 
-const screen = document.querySelector('#screen-output');
+// Listener and function call for non zero values 
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        appendScreen(number);
+    });
+});
 
-const buttons = document.querySelectorAll('button');
+// Listener and append for the value zero
+zero.addEventListener('click', () => {
+    if (screen.textContent.length > 8) {
+        return;
+    }
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        screen.textContent = button.id;
-    })
-})
+    if (screen.textContent[0] === '0' && screen.textContent[1] !== '.') {
+        return;
+    }
 
-screen.textContent = 0;
+    screen.append(zero.textContent);
+});
+
+// function to append non zero values
+function appendScreen(number) {
+    if (screen.textContent.length > 8) {
+        return;
+    }
+
+    if (screen.textContent[0] === '0' && !(screen.textContent.includes('.'))) {
+        screen.textContent = number.textContent;
+        return;
+    }
+
+    screen.append(number.textContent);
+}
+
+// Clears and sets the screen to zero
+clear.addEventListener('click', () => {
+    screen.textContent = '0';
+});
+
+period.addEventListener('click', () => {
+    if (screen.textContent.includes('.')) {
+        return;
+    }
+
+    screen.append(period.textContent);
+});
